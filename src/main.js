12b4d78867718a014479854efcbc5a4056b04cd7
jay <jayball@qq.com>
2021-08-17 14:51:38 +0800

    卸载vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: SummerJay__
  * @Date: 2021-08-11 22:29:33
- * @LastEditTime: 2021-08-15 10:46:04
+ * @LastEditTime: 2021-08-17 14:51:01
  * @LastEditors: your name
  * @Description:
  * @FilePath: \toutiao-publish-admin\src\main.js
@@ -9,7 +9,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 // 引入全局样式
 import '@/style/index.less'
 // 引入elementUi
@@ -25,6 +24,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
